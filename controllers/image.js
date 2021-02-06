bc45869003411edd4332b69bb37c60adcb606112
shneur <49290335+rubiginous@users.noreply.github.com>
2021-02-07 00:15:52 +0200

    key var

--- controllers/image.js
+++ controllers/image.js
@@ -1,6 +1,6 @@
 const clarifai = require('clarifai');
 const app = new Clarifai.App({
-    apiKey: "2c48e9078ded4426a89795af30e20197"
+    apiKey: process.env.apiKey
 });
 const handleApiCall = (req, res) => {
     app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
